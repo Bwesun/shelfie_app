52238012aaaecb40202492116614a5352618fad1
Innocent Matur <maturinnocent@gmail.com>
2026-05-28 02:14:59 +0100

    Add GuestOnly component to restrict access for authenticated users in AuthLayout

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -2,15 +2,16 @@ import { Stack } from 'expo-router'
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { useUser } from '../../hooks/useUser'
+import GuestOnly from '../../components/auth/GuestOnly'
 
 export default function AuthLayout() {
 
   const {user} = useUser()
   console.log('User: ', user)
   return (
-    <>
+    <GuestOnly>
         <StatusBar style="auto" />
         <Stack screenOptions={{ headerShown: false, animation: 'none' }} />
-    </>
+    </GuestOnly>
   )
 }
